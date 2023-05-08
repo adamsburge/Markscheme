9cc982e6fb7f0970a24f3ddca25e2f9bd7eb2383
adamsburge <93768863+adamsburge@users.noreply.github.com>
2023-05-08 14:47:00 +0100

    Create workshops.html and connect database content to page

--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from .models import Workshop, StudentResource, Product
+from django.conf import settings
+from django.db.models import Q
 
-# Create your views here.
+
+def workshops(request):
+    """ A view to show all workshops | The basic structure of this came from
+    Code Institute's Boutique Ado Project and has been modified here to fit
+     this project """
+
+    workshops = Product.objects.filter(Q(instance_of=Workshop))
+
+    context = {
+        'workshops': workshops,
+    }
+
+    return render(request, 'products/workshops.html', context)
