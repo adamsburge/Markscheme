61d54f2f5422401acff7b19b2aa58e1d0be24dab
adamsburge <93768863+adamsburge@users.noreply.github.com>
2023-05-08 12:47:12 +0100

    Add products models and admin setup

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from polymorphic.admin import PolymorphicParentModelAdmin, PolymorphicChildModelAdmin, PolymorphicChildModelFilter
+from .models import Category, Product, Workshop, StudentResource
 
-# Register your models here.
+
+class ModelAChildAdmin(PolymorphicChildModelAdmin):
+    """ Base admin class for all child models """
+    base_model = Product  # Optional, explicitly set here.
+
+
+@admin.register(Workshop)
+class ProductAdmin(ModelAChildAdmin):
+    base_model = Workshop  # Explicitly set here!
+    show_in_index = True  # makes child model admin visible in main admin site
+
+
+@admin.register(StudentResource)
+class ProductAdmin(ModelAChildAdmin):
+    base_model = StudentResource  # Explicitly set here!
+    show_in_index = True  # makes child model admin visible in main admin site
+
+
+@admin.register(Product)
+class ProductAdmin(PolymorphicParentModelAdmin):
+    base_model = Product  # Optional, explicitly set here.
+    child_models = (Workshop, StudentResource)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
+from polymorphic.models import PolymorphicModel
 
-# Create your models here.
+
+class Category(models.Model):
+    # Category model taken from Code Institute's Boutique Ado project
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(PolymorphicModel):
+    # Base of product model taken from Code Institute's Boutique Ado project,
+    # but adjusted here to fit the needs of this project
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    slug = models.SlugField(max_length=200, unique=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    host_creators = models.ManyToManyField(User, related_name='product_creators', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Workshop(Product):
+    location = models.CharField(max_length=150)
+    date = models.DateField()
+    time = models.TimeField()
+    total_slots = models.IntegerField()
+    available_slots = models.IntegerField()
+    attendance = models.ManyToManyField(User, related_name='workshop_attendees', blank=True)
+
+
+class StudentResource(Product):
+    file = models.FileField(upload_to='media/student_resources/')
+    owners = models.ManyToManyField(User, related_name='resource_owners', blank=True)
