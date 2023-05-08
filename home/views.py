85a76e517359c94e9b4114910993dbae3a4a4225
adamsburge <93768863+adamsburge@users.noreply.github.com>
2023-05-08 09:42:41 +0100

    Add products app and create about page

--- home/views.py
+++ home/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 def index(request):
     """ Returns Index page """
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """ Returns about page """
+    return render(request, 'home/about.html')
