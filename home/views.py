7df4bccc94d1657b9eeeaf59d689e4f1420bc55e
adamsburge <93768863+adamsburge@users.noreply.github.com>
2023-05-12 06:54:11 +0100

    Add staff page

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from profiles.models import UserProfile
 
 # Create your views here.
 
@@ -11,3 +12,15 @@ def index(request):
 def about(request):
     """ Returns about page """
     return render(request, 'home/about.html')
+
+
+def staff(request):
+    """ A view to show all the staff bios """
+
+    staff = UserProfile.objects.all()
+
+    context = {
+        'staff': staff,
+    }
+
+    return render(request, 'home/staff.html', context)
