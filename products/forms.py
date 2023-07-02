1a69b07fc2e016f7c4ce13c5e6ec19a67e0087f9
adamsburge <93768863+adamsburge@users.noreply.github.com>
2023-07-02 17:45:49 +0100

    Make form field hidden

--- products/forms.py
+++ products/forms.py
@@ -16,7 +16,8 @@ class WorkshopForm(forms.ModelForm):
     name = forms.CharField(label="Name of Workshop ")
     category = forms.ModelChoiceField(
         queryset=Category.objects.filter(name__startswith='w'),
-        initial=Category.objects.get(name__startswith='w').id
+        initial=Category.objects.get(name__startswith='w').id,
+        widget=forms.HiddenInput()
     )
     host_creators = CustomMMCF(
         queryset=User.objects.filter(is_superuser=True),
